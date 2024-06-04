ff637a235b8f53fe14fc3b34f1771bc362c4061c
Stefan Klinkert <s.klinkert@gmail.com>
2024-06-04 11:42:39 -0400

    done with DOM

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your code here!
+
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.append(newHeader);
+
+
+
+
+
+//const element = document.getElementById("main");
+//element.remove();
+
+
+
+
+
+
+//const ul = document.getElementsByTagName("main");
+
+
+
+//var2 = document.getElementByTagName("h1");
+
+
+
+//var1 = document.getElementById("main");
+//var1.remove();
+
+
+/*
+const h1name = document.getElementById("h1");
+h1name.textContent = "victory";
+
+const reMove = document.getElementsByTagName("main");
+reMove.remove();*/
